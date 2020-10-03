01ffdb980ec77255a1c3e31dd6c800d10a504b51
Patrick Müller <patrick.1999@web.de>
2020-10-03 20:55:24 +0200

    one class implementation, with rotation as helper for check_rotation(unfinished)

--- Objects.py
+++ Objects.py
@@ -5,62 +5,84 @@ Tetris
 import numpy as np
 
 
-class Object:
-    """
-    The superclass for the blocks in Tetris
-    """
+class Tetromino:
 
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
-
-    def rotate(self, vectors: list):
+    def __init__(self, tetro_type: str):
         """
-        The vectors are representing the squares of a tetris object.
-        The vectors are pointing to the top left corner of the squares,
-        which make up a whole tetris object.
-        The matrix represents a 90 degree rotation clockwise or
-        mathematically negativ rotation.
-        [ 0, 1]
-        [-1, 0]
+        The first element of the self.coords list is always the one in
+        the middle except for the o and l one.
+        Attributes:
+         - self.tetro_type : represents the tetromino
+         - self.box_coords : for determining where the tetromino is on
+                             the field
+         - self.coords     : the inner coordinates, inside the box, used
+                             for the rotation with matrix multiplication
         """
-        rotation_matrix = np.array([0, 1], [-1, 0])
-        for a in range(len(vectors)):
-            vectors[a] = rotation_matrix @ vectors[a]
+        self.tetro_type = tetro_type
+        if tetro_type == "o":
+            self.box_coords = [(4, 0), (5, 2)]
+            self.coords = [np.array([-0.5], [-0.5]), np.array([-0.5], [0.5]),
+                           np.array([0.5], [0.5]), np.array([0.5], [-0.5])]
+        elif tetro_type == "L":
+            self.box_coords = [(3, 0), (5, 3)]
+            self.coords = [np.array([0], [0]), np.array([-0.5], [0]),
+                           np.array([0.5], [0]), np.array([0.5], [0.5])]
+        elif tetro_type == "J":
+            self.box_coords = [(3, 0), (5, 3)]
+            self.coords = [np.array([0], [0]), np.array([-0.5], [0]),
+                           np.array([-0.5], [0.5]), np.array([0.5], [0])]
+        elif tetro_type == "z":
+            self.box_coords = [(3, 0), (5, 3)]
+            self.coords = [np.array([0], [0]), np.array([-0.5], [0.5]),
+                           np.array([0], [0.5]), np.array([0.5], [0])]
+        elif tetro_type == "s":
+            self.box_coords = [(3, 0), (5, 3)]
+            self.coords = [np.array([0], [0]), np.array([-0.5], [0]),
+                           np.array([0], [0.5]), np.array([0.5], [0.5])]
+        elif tetro_type == "T":
+            self.box_coords = [(3, 0), (5, 3)]
+            self.coords = [np.array([0], [0]), np.array([-0.5], [0]),
+                           np.array([0], [0.5]), np.array([0.5], [0])]
+        elif tetro_type == "l":
+            self.box_coords = [(3, 0), (6, 4)]
+            self.coords = [np.array([-1.5], [-0.5]), np.array([-0.5], [-0.5]),
+                           np.array([0.5], [-0.5]), np.array([1.5], [-0.5])]
 
-    def move_right(self):
-        self.x = self.x + 1
+    def rotation(self, direction: bool) -> list:
+        """
+        The rotation is processed through matrix multiplication.
+        The vectors of self.coords are either multiplied with a matrix
+        indicating clockwise rotation for direction = True
+        and counterclockwise rotation for direction = False
 
-    def move_left(self):
-        self.x = self.x - 1
+        This function returns a list with the vectors after the
+        matrix multiplication. This is needed for checking if the
+        rotation was possible and then afterwards applying the
+        rotation to the real self.coords
+        Basically this is a helper function
+        """
+        result = []
+        clockwise = np.array([0, 1], [-1, 0])
+        counterclockwise = np.array([0, -1], [1, 0])
 
-    def move_down(self):
-        self.y = self.y + 1
+        if direction is True:
+            for a in self.coords:
+                result = result + [clockwise @ a]
+        else:
+            for a in self.coords:
+                result = result + [counterclockwise @ a]
 
+        return result
 
-class Tetriminoes(Object):
+    def check_rotation(self, direction: bool):
+        """
+        This function checks if the rotation was possible,
+        with wall kicks and applies them if possible.
+        """
+        # find out how the rotations work
 
-    def __init__(self, x: int, y: int, block_type: int):
-        super().__init__(x, y)
-        if block_type == 0:
-            # the o
-            self.vectors = [(0, 0), (-1, 0), (-1, 1), (0, 1)]
-        elif block_type == 1:
-            # the z
-            self.vectors = [(0, 0), (-1, 1), (0, 1), (1, 0)]
-        elif block_type == 2:
-            # the s
-            self.vectors = [(0, 0), (-1, 0), (0, 1), (1, 1)]
-        elif block_type == 3:
-            # the L
-            self.vectors = [(0, 0), (-1, 0), (1, 1), (1, 0)]
-        elif block_type == 4:
-            # the j
-            self.vectors = [(0, 0), (-1, 0), (-1, 1), (1, 0)]
-        elif block_type == 5:
-            # the T
-            self.vectors = [(0, 0), (-1, 0), (0, 1), (1, 0)]
-        elif block_type == 6:
-            # the l
-            self.vectors = [(-1.5, -0.5), (-0.5, -0.5),
-                            (0.5, -0.5), (1.5, -0.5)]
+# make a function that returns the real
+# coords of the tiles of a tetromino
+# by taking the self.coords and turn
+# them into real coords with the self.box_coords
+# make a field with dictionary
